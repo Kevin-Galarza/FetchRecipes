b4c11933f164b1e776243fce3e7d1920fc7c2620
Kevin Galarza <k.galarza@me.com>
2024-12-23 11:18:28 -0500

    Initial Commit

---
+++ FetchRecipeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FetchRecipeApp
+//
+//  Created by Kevin Galarza on 12/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
